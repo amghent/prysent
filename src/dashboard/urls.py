7e695df47de4eaf8e99c363c2ca33fa3dc45ac00
Yves Vindevogel <yves.vindevogel.external@arcelormittal.com>
2022-08-04 19:55:52 +0200

    FIX: Removed the light sidebar example, left a reverse url

--- src/dashboard/urls.py
+++ src/dashboard/urls.py
@@ -6,7 +6,6 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("index.html", views.index, name="index"),
     path("layout-static.html", views.layout_static, name="layout_static"),
-    path("layout-sidenav-light.html", views.layout_sidenav_light, name="layout_sidenav_light"),
     path("login.html", views.login, name="login"),
     path("password.html", views.password, name="password"),
     path("register.html", views.register, name="register"),
