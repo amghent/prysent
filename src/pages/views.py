178d4455b6bf697cac304dcbcdec505163abf5ae
Yves Vindevogel <yves.vindevogel.external@arcelormittal.com>
2022-08-09 20:22:49 +0200

    Implemented authorisation with fully working login page

--- src/pages/views.py
+++ src/pages/views.py
@@ -1,22 +1,23 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
+from dashboard.context import Context
+
 
 def public_page(request, slug: str):
-    context = {
-        "notebook": slug,
-        "internal": True
-    }
+    context = Context(request=request).get()
+
+    context["notebook"] = slug,
+    context["internal"] = True
 
     return render(request=request, template_name="dashboard/notebook.jinja2", context=context)
 
 
 @login_required
 def authorized_page(request, slug: str):
-    context = {
-        "notebook": slug,
-        "internal": True
-    }
+    context = Context(request=request).get()
+
+    context["notebook"] = slug,
+    context["internal"] = True
 
     return render(request=request, template_name="dashboard/notebook.jinja2", context=context)
-
