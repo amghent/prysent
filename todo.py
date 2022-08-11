ef3b6db2a8b695aa633b60f358b61b0c7f13e444
Yves Vindevogel <yves.vindevogel.external@arcelormittal.com>
2022-08-11 22:41:06 +0200

    Updated todo

--- todo.py
+++ todo.py
@@ -1,8 +1,9 @@
-# FIXME: rows will contain all the cardboxes, without taking the row into account
+# FIXME: multiple rows of cardboxes do not stack well in rows
 # FIXME: sortorder of the cardboxes is not yet implemented
-# FIXME: the selected menus are not opened again after clicking a menu
 # FIXME: received mail from GitGuard that the secret key is in the settings
 
 # TODO: overflow property on a cardbox
 # TODO: datapage without box
 # TODO: datapage with full height iframe
+
+# FIXME: the selected menus are not opened again after clicking a menu (requires javascript, it seems)
