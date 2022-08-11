3958d79fecd3763103a1099758e65335bd7fabfd
Yves Vindevogel <yves.vindevogel.external@arcelormittal.com>
2022-08-11 16:23:13 +0200

    Updated todo

--- todo.py
+++ todo.py
@@ -1,4 +1,3 @@
-# FIXME: remove the level + type debug in context
 # FIXME: rows will contain all the cardboxes, without taking the row into account
 # FIXME: sortorder of the cardboxes is not yet implemented
 # FIXME: the selected menus are not opened again after clicking a menu
@@ -8,3 +7,4 @@
 # TODO: remove the need for putting ids in the sidebar, auto-generate them from jinja2 template
 # TODO: make command to auto-generate the directory structure
 # TODO: overflow property on a cardbox
+# TODO: datapage without box
