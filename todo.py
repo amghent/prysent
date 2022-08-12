3025e557328d5e2220d449b3f6a42e05319daf4a
Yves Vindevogel <yves.vindevogel.external@arcelormittal.com>
2022-08-12 19:59:34 +0200

    Updated todo list

--- todo.py
+++ todo.py
@@ -4,9 +4,12 @@
 # TODO: Microsoft SQL server as backend
 # TODO: add title on a datapage
 # TODO: sidebar menus must be smaller
-
+# TODO: vars for height of the footer and the nav bar, to use in calc()
 # TODO: check if we can position the content of the iframe, would be nice to correct margins
+# TODO: order of the menus must be changeable
+
 # TODO: FEATURE: upload files
 # TODO: FEATURE: single sign on
 # TODO: FEATURE: logging to file
 # TODO: FEATURE: stats on the application itself
+# TODO: FEATURE: caching
