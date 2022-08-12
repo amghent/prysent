5da07e68ca7989cb4f60b6cb51f21cad4e02f312
Yves Vindevogel <yves.vindevogel.external@arcelormittal.com>
2022-08-12 17:55:31 +0200

    Extra property to hide possible overflow in the iframe

--- todo.py
+++ todo.py
@@ -1,5 +1,7 @@
 # FIXME: received mail from GitGuard that the secret key is in the settings
 
-# TODO: overflow property on a cardbox
+# TODO: Microsoft SQL server as backend
 # TODO: datapage without box
 # TODO: datapage with full height iframe
+
+# TODO: check if we can position the content of the iframe, would be nice to correct margins
