50827098869a3d70da36e0a69d196e3dafd7bd71
Yves Vindevogel <yves.vindevogel.external@arcelormittal.com>
2022-08-12 19:54:37 +0200

    Unboxed cards (to display the notebook as such)

--- todo.py
+++ todo.py
@@ -1,10 +1,12 @@
 # FIXME: received mail from GitGuard that the secret key is in the settings
+# FIXME: in the admin pages, some of the tables are shown with spelling errors (box -> boxes)
 
 # TODO: Microsoft SQL server as backend
-# TODO: datapage without box
 # TODO: add title on a datapage
+# TODO: sidebar menus must be smaller
 
 # TODO: check if we can position the content of the iframe, would be nice to correct margins
-
 # TODO: FEATURE: upload files
 # TODO: FEATURE: single sign on
+# TODO: FEATURE: logging to file
+# TODO: FEATURE: stats on the application itself
