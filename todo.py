704b9987685b7d49e58d981c8adb92ddca888061
Yves Vindevogel <yves.vindevogel.external@arcelormittal.com>
2022-08-22 14:51:42 +0200

    Updated todo

--- todo.py
+++ todo.py
@@ -1,8 +1,9 @@
+# TODO: role of manager for an organisational unit
 # TODO: FEATURE: upload files
+# FIXME: seems to be a gap on the home page where the iframe is not visible
 # TODO: FEATURE: single sign on
 # TODO: FEATURE: logging to file
 # TODO: FEATURE: stats on the application itself
 # TODO: FEATURE: caching
-# TODO: FEATURE: add itables to the possibilities for the notebooks
 # TODO: FEATURE: docker container
 # TODO: FEATURE: write install guide
