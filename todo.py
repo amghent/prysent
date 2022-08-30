e927ef2061d6ca55d16175b7ee962fc8e8b38d94
Yves Vindevogel <yves.vindevogel.external@arcelormittal.com>
2022-08-30 15:34:01 +0200

    Updated TODO with some minor bugs

--- todo.py
+++ todo.py
@@ -1,8 +1,11 @@
+# FIXME: the spacing for data and notebook is not the same
+# FIXME: size of the --notebook-height var in css
+# FIXME: bottom bar is too high
+
 # TODO: FEATURE: upload files
 # TODO: FEATURE: single sign on
 # TODO: FEATURE: logging to file
 # TODO: FEATURE: stats on the application itself
 # TODO: FEATURE: caching
-# TODO: FEATURE: add itables to the possibilities for the notebooks
 # TODO: FEATURE: docker container
 # TODO: FEATURE: write install guide
