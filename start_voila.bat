02996e72b1656706af2e68185d1e3a6f29322d12
Yves Vindevogel <yves.vindevogel.external@arcelormittal.com>
2022-09-02 12:12:38 +0200

    Added batch files for easier startup on Windows

---
+++ start_voila.bat
@@ -0,0 +1,3 @@
+CALL activate prysent2204
+
+voila "C:\Users\SIDSTA1\ArcelorMittal\AM Python - Documenten\Jobs\Automatic_notebooks" --port=8876 --no-browser --Voila.ip="0.0.0.0" --Voila.tornado_settings="{'headers':{'Content-Security-Policy': 'frame-ancestors http://svsim1hl.sidmar.be:8875'}}"
---
+++ update_menu.bat
@@ -0,0 +1,3 @@
+CALL activate prysent2204
+
+python .\src\manage.py upload_media --settings=prysent.settings.sqlite3
