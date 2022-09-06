80d24c3087f3cb36d9a8b6c48ac5701b4cfe46e9
Yves Vindevogel <yves.vindevogel.external@arcelormittal.com>
2022-09-06 13:27:21 +0200

    Added fixme to TODO in order not to forget

--- todo.py
+++ todo.py
@@ -1,3 +1,5 @@
+# FIXME: scrollbar seems to appear in some cases (maybe on larger menus)
+
 # TODO: FEATURE: logging to file
 # TODO: FEATURE: stats on the application itself
 # TODO: FEATURE: caching
