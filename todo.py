59f432835a482f8744d0b213bd46083b22f3347c
Yves Vindevogel <yves.vindevogel.external@arcelormittal.com>
2022-09-20 17:45:49 +0200

    Updated todo after new release

--- todo.py
+++ todo.py
@@ -1,3 +1,8 @@
+# FIXME: Lost print command "Need notebook"
+
+# FIXME: "Afwijking I3 Bedding" does not show graph
+
+# TODO: FEATURE: animated gif for waiting
 # TODO: FEATURE: clean cache via command url (/commands/clean-cache)
 # TODO: FEATURE: update the menu via command url (/commands/update-menu)
 # TODO: FEATURE: run the upload of the scheduler via command url (/commands/update-schedule)
